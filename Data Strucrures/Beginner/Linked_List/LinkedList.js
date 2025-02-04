74d67d46b54adf44139b9f6ac71f87a663b80931
GoutamK1902 <goutamkhowal321@gmail.com>
2025-02-04 15:19:46 +0530

    Linked Lists Summing Up

--- Data Strucrures/Beginner/Linked_List/LinkedList.js
+++ Data Strucrures/Beginner/Linked_List/LinkedList.js
@@ -5,11 +5,19 @@ class Node {
   }
 }
 
+class Compare {
+  equal(a, b) {
+    return a === b;
+  }
+}
+
 class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
   }
+  //*Compare*/
+  compare = new Compare();
 
   //*Insert*/
   /*-----Pseudocode for Adding at end (appending)-----*/
@@ -96,6 +104,36 @@ class LinkedList {
   //
   //  RETURN this // Return the updated linked list instance
   // end InsertAt
+  InsertAt(value, rawIndex) {
+    const index = rawIndex < 0 ? 0 : rawIndex;
+    if (index == 0) {
+      this.Prepend(value); //Insert at head (of List)
+    } else {
+      let count = 1;
+      let currentNode = this.head;
+      const newNode = new Node(value);
+
+      while (currentNode != null) {
+        if (count === index) break;
+        currentNode = currentNode.next;
+        count = count + 1;
+      }
+
+      if (currentNode != null) {
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+      } else {
+        if (this.tail !== null) {
+          this.tail.next = newNode;
+          this.tail = newNode;
+        } else {
+          this.head = newNode;
+          this.tail = newNode;
+        }
+      }
+    }
+    return this;
+  }
 
   //*Search*/
   /*-----Pseudocode for Search-----*/
@@ -235,6 +273,195 @@ class LinkedList {
       console.log(currentNode.value);
     }
   }
+
+  //*Reverse*/
+  /*-----Pseudocode for Reverse-----*/
+  //   Reverse()
+  //  Pre: The linked list is non-empty
+  //  Post: The linked list is reversed
+  //  currNode ← head // Start with the head of the list
+  //  prevNode ← ø // Initialize previous node as null
+  //  nextNode ← ø // Initialize next node as null
+  //
+  //  WHILE currNode ≠ ø DO
+  //      nextNode ← currNode.next // Store the next node
+  //      currNode.next ← prevNode // Reverse the link
+  //      prevNode ← currNode // Move prevNode one step forward
+  //      currNode ← nextNode // Move currNode one step forward
+  //  END WHILE
+  //
+  //  tail ← head // Update tail to the original head
+  //  head ← prevNode // Update head to the new head (prevNode)
+  //
+  //  RETURN this // Return the updated linked list instance
+  // end Reverse
+  Reverse() {
+    let currentNode = this.head;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currentNode) {
+      nextNode = currentNode.next;
+      currentNode.next = prevNode;
+      prevNode = currentNode;
+      currentNode = nextNode;
+    }
+    this.tail = this.head;
+    this.head = prevNode;
+    return this;
+  }
+  //*To Array*/
+  /*-----Pseudocode for ToArray-----*/
+  //   ToArray()
+  //  Pre: The linked list is non-empty or empty
+  //  Post: An array containing all nodes of the linked list is returned
+  //  nodes ← [] // Initialize an empty array to hold the nodes
+  //  currentNode ← head // Start with the head of the list
+  //
+  //  WHILE currentNode ≠ ø DO
+  //      nodes.push(currentNode) // Add the current node to the array
+  //      currentNode ← currentNode.next // Move to the next node
+  //  END WHILE
+  //
+  //  RETURN nodes // Return the array of nodes
+  // end ToArray
+  ToArray() {
+    let nodes = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+    return nodes;
+  }
+
+  //*From Array*/
+  /*-----Pseudocode for FromArray-----*/
+  //   FromArray(values)
+  //  Pre: values is an array of values to add to the linked list
+  //  Post: The linked list is populated with the values from the array
+  //
+  //  FOR EACH value IN values DO
+  //      CALL Append(value) // Append each value to the linked list
+  //  END FOR
+  //
+  //  RETURN this // Return the updated linked list instance
+  // end FromArray
+  FromArray(values) {
+    values.forEach((value) => {
+      this.Add(value);
+    });
+    return this;
+  }
+
+  //*DeleteHead*/
+  /*-----Pseudocode for DeleteHead-----*/
+  //   DeleteHead()
+  //  Pre: The linked list may be empty or contain nodes
+  //  Post: The head node is removed from the linked list, and the deleted node is returned
+  //
+  //  IF head = ø THEN
+  //      RETURN ø // If the list is empty, return null
+  //  END IF
+  //
+  //  deletedHead ← head // Store the current head node
+  //
+  //  IF head.next ≠ ø THEN
+  //      head ← head.next // Update head to the next node
+  //  ELSE
+  //      head ← ø // Set head to null (list is now empty)
+  //      tail ← ø // Set tail to null (list is now empty)
+  //  END IF
+  //
+  //  RETURN deletedHead // Return the removed head node
+  // end DeleteHead
+  DeleteHead() {
+    if (!this.head) {
+      return null;
+    }
+    const deleteHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deleteHead;
+  }
+
+  //*DeleteTail*/
+  /*-----Pseudocode for DeleteTail-----*/
+  DeleteTail() {
+    const deletedTail = this.tail;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (currentNode.next.next == null) {
+        currentNode.next = null;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    this.tail = currentNode;
+
+    return deletedTail;
+  }
+
+  //*Find*/
+  /*-----Pseudocode for Find-----*/
+  //   Find({ value = undefined, callback = undefined })
+  //  Pre: The linked list may be empty or contain nodes
+  //  Post: Returns the first node that matches the specified value or callback, or null if not found
+  //
+  //  IF head = ø THEN
+  //      RETURN ø // If the list is empty, return null
+  //  END IF
+  //
+  //  currentNode ← head // Start from the head of the list
+  //
+  //  WHILE currentNode ≠ ø DO
+  //      // If a callback is specified, check if it matches the current node's value
+  //      IF callback ≠ ø AND callback(currentNode.value) THEN
+  //          RETURN currentNode // Return the current node if callback matches
+  //      END IF
+  //
+  //      // If a value is specified, check if it matches the current node's value
+  //      IF value ≠ undefined AND compare.equal(currentNode.value, value) THEN
+  //          RETURN currentNode // Return the current node if value matches
+  //      END IF
+  //
+  //      currentNode ← currentNode.next // Move to the next node
+  //  END WHILE
+  //
+  //  RETURN ø // Return null if no matching node is found
+  // end Find
+
+  Find({ value = undefined, callback = undefined }) {
+    if (this.head == null) {
+      return null;
+    }
+    let currentNode = this.head;
+    while (currentNode) {
+      if (callback && callback(currentNode.value)) {
+        return currentNode;
+      }
+
+      if (value !== undefined && this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
 }
 
 const MyLinkedList = new LinkedList();
@@ -261,3 +488,34 @@ console.log("\n\n**************\nTraversing the Linked List");
 MyLinkedList.Traverse();
 console.log("\n\n**************\nReverse Traversing the Linked List");
 MyLinkedList.ReverseTraverse();
+MyLinkedList.InsertAt(50000, 2);
+MyLinkedList.InsertAt(50005, 3);
+console.log("\n\n**************\nTraversing the Linked List");
+MyLinkedList.Traverse();
+
+console.log("Original");
+console.log(JSON.stringify(MyLinkedList));
+
+MyLinkedList.Reverse();
+console.log("Reversed");
+console.log(JSON.stringify(MyLinkedList));
+
+console.log(MyLinkedList.ToArray());
+
+console.log();
+MyLinkedList.FromArray([5, 10, 15, 20, "Goutam", 25]);
+MyLinkedList.Traverse();
+
+MyLinkedList.DeleteHead();
+console.log();
+MyLinkedList.Traverse();
+console.log();
+console.log();
+MyLinkedList.DeleteTail();
+MyLinkedList.Traverse();
+console.log();
+console.log();
+
+console.log(MyLinkedList.Find({ value: "Goutam" }));
+MyLinkedList.Delete("Goutam");
+console.log(MyLinkedList.Find({ value: "Goutam" }));
