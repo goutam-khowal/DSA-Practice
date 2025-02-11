64496c1e739d5f46306c1e879055dc07b5b94fd6
GoutamK1902 <goutamkhowal321@gmail.com>
2025-02-11 14:43:52 +0530

    Done With DLL

--- Data Strucrures/Beginner/Doubly_Linked_List/DoublyLinkedList.js
+++ Data Strucrures/Beginner/Doubly_Linked_List/DoublyLinkedList.js
@@ -72,6 +72,35 @@ class DoublyLinkedList {
     return values.join(" <-> ");
   }
 
+  //*ToStringCustom()*/
+  /*-----Pseudocode for ToStringCustom(callback)-----*/
+  // ToStringCustom(callback)
+  // Pre: callback is a function that defines how to convert a node to a string
+  // Post: Returns a string representation of the list using the provided callback
+
+  // Call this.ToArray() to get an array of nodes
+  // Initialize an array called nodeStrings
+
+  // FOR EACH node IN the array of nodes WITH index DO
+  //     IF the length of callback is less than 2 THEN
+  //         Call callback(node) and add the result to nodeStrings
+  //     ELSE
+  //         Call callback(node, index) and add the result to nodeStrings
+  //     END IF
+  // END FOR
+
+  // Convert the nodeStrings array to a single string by joining elements with ", "
+  // RETURN the resulting string
+  // end ToStringCustom
+
+  ToStringCustom(callback) {
+    return this.ToArray()
+      .map((node, index) => {
+        return callback.length < 2 ? callback(node) : callback(node, index);
+      })
+      .join(", ");
+  }
+
   //*ReverseTraverse*/
   /*-----Pseudocode for ReverseTraversal---*/
   //   ReverseTraverse()
@@ -96,6 +125,62 @@ class DoublyLinkedList {
       console.log(currentNode.value);
       currentNode = currentNode.previous;
     }
+  }
+
+  //*reverse()*/
+  /*-----Pseudocode for reverse()-----*/
+  // reverse()
+  // Pre: The doubly linked list may be empty or contain nodes
+  // Post: Reverses the order of the nodes in the list
+
+  // Initialize currNode to this.head
+  // currNode ← this.head
+
+  // Initialize prevNode to null
+  // prevNode ← null
+
+  // Initialize nextNode to null
+  // nextNode ← null
+
+  // WHILE currNode IS NOT null DO
+  //     // Store the next node
+  //     nextNode ← currNode.next
+  //     prevNode ← currNode.previous
+
+  //     // Change the next and previous pointers of the current node
+  //     currNode.next ← prevNode
+  //     currNode.previous ← nextNode
+
+  //     // Move prevNode and currNode nodes one step forward
+  //     prevNode ← currNode
+  //     currNode ← nextNode
+  // END WHILE
+
+  // // Reset head and tail
+  // this.tail ← this.head
+  // this.head ← prevNode
+
+  // RETURN this // Return the updated list
+  // end reverse
+  Reverse() {
+    let currentNode = this.head;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currentNode) {
+      nextNode = currentNode.next;
+      prevNode = currentNode.previous;
+
+      currentNode.next = prevNode;
+      currentNode.previous = nextNode;
+
+      prevNode = currentNode;
+      currentNode = nextNode;
+    }
+    this.tail = this.head;
+    this.head = prevNode;
+
+    return this;
   }
 
   //*Insert*/
@@ -237,6 +322,64 @@ class DoublyLinkedList {
     return null;
   }
 
+  //*DeleteRange()*/
+  /*-----Pseudocode for DeleteRange(fromIndex, toIndex)-----*/
+  // DeleteRange(fromIndex, toIndex)
+  // Pre: fromIndex and toIndex are expected to be numbers
+  // Post: Deletes nodes in the range from fromIndex to toIndex and returns the updated list
+
+  // IF fromIndex is NOT a number OR toIndex is NOT a number THEN
+  //     RETURN null
+  // END IF
+
+  // IF fromIndex > toIndex THEN
+  //     RETURN null
+  // END IF
+
+  // Initialize count to 0
+  // Initialize currentNode to this.head
+  // Initialize nodesToDelete as an empty array
+
+  // WHILE currentNode IS NOT null DO
+  //     IF count >= fromIndex AND count <= toIndex THEN
+  //         Add currentNode to nodesToDelete
+  //     END IF
+  //     Increment count
+  //     Move currentNode to currentNode.next
+  // END WHILE
+
+  // FOR EACH node IN nodesToDelete DO
+  //     Call this.Delete(node.value)
+  // END FOR
+
+  // RETURN this // Return the updated list
+  // end DeleteRange
+  DeleteRange(fromIndex, toIndex) {
+    if (typeof fromIndex !== "number" || typeof toIndex !== "number") {
+      return null;
+    }
+
+    if (fromIndex > toIndex) {
+      return null;
+    }
+
+    let count = 0;
+    let currentNode = this.head;
+    let nodesToDelete = [];
+
+    while (currentNode) {
+      if (count >= fromIndex && count <= toIndex) {
+        nodesToDelete.push(currentNode);
+      }
+      count++;
+      currentNode = currentNode.next;
+    }
+
+    nodesToDelete.forEach((node) => this.Delete(node.value));
+
+    return this;
+  }
+
   //*DeleteTail()*/
   /*-----Pseudocode for DeleteTail()-----*/
   //   DeleteTail()
@@ -323,6 +466,123 @@ class DoublyLinkedList {
 
     return deletedHead;
   }
+
+  //*find()*/
+  /*-----Pseudocode for find({value, callback})-----*/
+  // find({ value = undefined, callback = undefined })
+  // Pre: The doubly linked list may be empty or contain nodes
+  // Post: Returns the first node that matches the specified value or callback, or null if not found
+
+  // IF this.head IS null THEN
+  //     RETURN null // List is empty
+  // END IF
+
+  // currentNode ← this.head // Start from the head of the list
+
+  // WHILE currentNode IS NOT null DO
+  //     // Check if both value and callback are provided
+  //     IF callback IS NOT undefined AND value IS NOT undefined THEN
+  //         IF this.compare.equal(currentNode.value, value) AND callback(currentNode.value) THEN
+  //             RETURN currentNode // Return if both conditions are met
+  //         END IF
+  //     END IF
+
+  //     // Check if only callback is provided
+  //     IF callback IS NOT undefined AND callback(currentNode.value) THEN
+  //         RETURN currentNode // Return if callback returns true
+  //     END IF
+
+  //     // Check if only value is provided
+  //     IF value IS NOT undefined AND this.compare.equal(currentNode.value, value) THEN
+  //         RETURN currentNode // Return if value matches
+  //     END IF
+
+  //     currentNode ← currentNode.next // Move to the next node
+  // END WHILE
+
+  // RETURN null // No matching node found
+  // end find
+  find({ value = undefined, callback = undefined }) {
+    if (this.head === null) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (callback !== undefined && value !== undefined) {
+        if (
+          this.compare.equal(currentNode.value, value) &&
+          callback(currentNode.value)
+        ) {
+          return currentNode;
+        }
+      } else if (callback !== undefined && callback(currentNode.value)) {
+        return currentNode;
+      } else if (
+        value !== undefined &&
+        this.compare.equal(currentNode.value, value)
+      ) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  //*ToArray()*/
+  /*-----Pseudocode for ToArray()-----*/
+  // toArray()
+  // Pre: The doubly linked list may be empty or contain nodes
+  // Post: Returns an array of nodes in the list
+
+  // Initialize an empty array to hold the nodes
+  //  nodes ← []
+
+  // Set currentNode to the head of the list
+  //  currentNode ← this.head
+
+  // WHILE currentNode IS NOT null DO
+  //     Add the current node to the nodes array
+  //     nodes.push(currentNode)
+  //
+  //     // Move to the next node
+  //     currentNode ← currentNode.next
+  // END WHILE
+
+  // RETURN nodes // Return the array of nodes
+  // end toArray
+  ToArray() {
+    let nodes = [];
+    let currentNode = this.head;
+
+    while (currentNode) {
+      nodes.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  //*fromArray()*/
+  /*-----Pseudocode for fromArray(values)-----*/
+  // fromArray(values)
+  // Pre: values is an array of elements to be added to the doubly linked list
+  // Post: Returns the updated doubly linked list
+
+  // FOR EACH value IN values DO
+  //     Call this.append(value) // Append each value to the list
+  // END FOR
+
+  // RETURN this // Return the updated list
+  // end fromArray
+  FromArray(values) {
+    values.forEach((value) => {
+      this.Append(value);
+    });
+    return this;
+  }
 }
 
 const MyList = new DoublyLinkedList();
@@ -330,16 +590,93 @@ MyList.Append("Third");
 MyList.Prepend("Second");
 MyList.Append("Fourth");
 MyList.Prepend("First");
+
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+console.log("List after initial insertions:");
 console.log(MyList.ToString());
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+
 MyList.Delete("Third");
 MyList.Delete("Second");
 
+console.log("Traversing the list:");
 MyList.Traverse();
-console.log();
-MyList.ReverseTraverse();
-console.log();
-console.log("deleted tail: " + JSON.stringify(MyList.DeleteTail()));
-console.log();
-console.log(MyList.ToString());
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
 
-//27 minutes
+console.log("Reverse Traversing the list:");
+MyList.ReverseTraverse();
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+
+console.log("Deleting tail element:");
+console.log("Deleted Tail: " + JSON.stringify(MyList.DeleteTail()));
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+
+console.log("List after deleting tail:");
+console.log(MyList.ToString());
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+
+MyList.Append(12);
+MyList.Append(120);
+MyList.Append(180);
+MyList.Append(60);
+MyList.Append(119);
+
+console.log("List after appending new numbers:");
+console.log(MyList.ToArray());
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+
+console.log("Finding first even number:");
+console.log(
+  MyList.find({
+    callback: (value) => value % 2 === 0,
+  })
+);
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+
+// FUNCTION IN FUNCTION (CLOSURE)
+function specificEven(targetValue) {
+  return function check(value) {
+    return value % 2 === 0 && value === targetValue;
+  };
+}
+
+console.log("Finding specific even number (60):");
+console.log(
+  MyList.find({
+    callback: specificEven(60),
+  })
+);
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+
+console.log("Finding even number with value 180:");
+console.log(
+  MyList.find({
+    value: 180,
+    callback: (value) => value % 2 === 0,
+  })
+);
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+console.log("List After deleting Range (0-5):");
+
+MyList.DeleteRange(0, 5);
+console.log(MyList.ToArray());
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+
+MyList.FromArray([12, 24, 36, 48, 60, 72, 84, 96, 108, 120]);
+console.log(MyList.ToArray());
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+console.log("normal to string");
+console.log(MyList.ToString());
+console.log("custom to string");
+console.log(
+  MyList.ToStringCustom((value, index) => {
+    return `Node ${index + 1}: ${value}`;
+  })
+);
+console.log("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
+console.log("List before reversing");
+console.log(MyList.ToString());
+MyList.Reverse();
+console.log("List after reversing");
+console.log(MyList.ToString());
+("\n------L-I-N-E--O-F--S-E-P-A-R-A-T-I-O-N------\n");
